a88a9669ed87960804bf537efd2eab3f971b17f9
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-01 00:43:02 +0530

    feat: added get conversion rate api function

--- src/services/currencies.ts
+++ src/services/currencies.ts
@@ -1,8 +1,28 @@
-import { LIST_CURRENCIES_PATH } from '../consts/urls';
+import { GET_CONVERSION_RATE_PATH, LIST_CURRENCIES_PATH } from '../consts/urls';
 
 export const listCurrencies = async () => {
   try {
     const res = await fetch(LIST_CURRENCIES_PATH);
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    return { error: 'Something went wrong!' };
+  }
+};
+
+export const getConversionRate = async ({
+  source,
+  amount,
+  target,
+}: {
+  source: string;
+  amount: number;
+  target: string;
+}) => {
+  try {
+    const res = await fetch(
+      `${GET_CONVERSION_RATE_PATH}?source=${source}&amount=${amount}&target=${target}`
+    );
     const result = await res.json();
     return result;
   } catch (err) {
