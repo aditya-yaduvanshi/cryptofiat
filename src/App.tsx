d1849d73a17dbbca1cf2913ae10b2db49467b86f
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-01 00:40:59 +0530

    feat: added index page in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Index from './pages/index';
 
 const App = () => {
   return (
     <>
-      <div className="w-screen h-screen">
+      <div className="w-screen h-screen overflow-hidden">
         <Header />
-        APP
+        <Index />
       </div>
     </>
   );
