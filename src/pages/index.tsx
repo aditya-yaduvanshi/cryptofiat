5bafad71611dfbe5254f4af908c9506547475406
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-02 02:44:11 +0530

    refactor: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,46 +5,44 @@ import { useConversionRates } from '../contexts/conversion';
 
 const Index = () => {
   const [isShowModal, setShowModal] = useState(false);
-  const { conversions, getConversionRate, removeConversion } =
+  const { conversions, removeConversion, syncConversion } =
     useConversionRates();
 
   return (
     <>
-      <main className="flex flex-col gap-5 p-5 md:gap-0 w-full h-[calc(100vh-98px)] overflow-y-auto overflow-x-hidden">
+      <main className="container mx-auto flex flex-col gap-5 p-5 w-full h-[calc(100vh-98px)] overflow-y-auto overflow-x-hidden">
         {conversions.length ? (
-          <button
-            type="button"
-            onClick={() => setShowModal(true)}
-            className="w-full hover:bg-blue-500 transition-all duration-200 active:scale-90 flex items-center justify-center bg-blue-700 py-2 text-white rounded"
-          >
-            New Conversion
-          </button>
-        ) : null}
-        <ul className="h-full w-full">
-          {conversions.length ? (
-            <>
+          <>
+            <button
+              type="button"
+              onClick={() => setShowModal(true)}
+              className="w-1/2 mx-auto font-semibold hover:bg-blue-500 transition-all duration-200 active:scale-90 flex items-center justify-center bg-blue-700 py-2 text-white rounded"
+            >
+              New Conversion
+            </button>
+            <ul className="h-full w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2.5">
               {conversions.map((conversion) => (
                 <ConversionCard
                   key={conversion.id}
                   conversion={conversion}
-                  onSync={getConversionRate}
+                  onSync={syncConversion}
                   onRemove={removeConversion}
                 />
               ))}
-            </>
-          ) : (
-            <li className="flex flex-col justify-center items-center h-full text-2xl gap-5">
-              No recent conversions to show.
-              <button
-                type="button"
-                onClick={() => setShowModal(true)}
-                className="bg-blue-700 hover:bg-blue-500 transition-all duration-200 active:scale-90 flex items-center justify-center px-5 py-2 text-white rounded"
-              >
-                New Conversion
-              </button>
-            </li>
-          )}
-        </ul>
+            </ul>
+          </>
+        ) : (
+          <div className="flex flex-col justify-center items-center mx-auto h-full text-2xl gap-5">
+            No recent conversions to show.
+            <button
+              type="button"
+              onClick={() => setShowModal(true)}
+              className="bg-blue-700 hover:bg-blue-500 transition-all duration-200 active:scale-90 flex items-center justify-center px-5 py-2 text-white rounded"
+            >
+              New Conversion
+            </button>
+          </div>
+        )}
       </main>
       <NewConversionModal
         isShow={isShowModal}
